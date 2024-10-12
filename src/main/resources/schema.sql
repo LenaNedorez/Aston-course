4c5a4ce53df1cd4192f578dd55316b8d08f267f5
Elena Nedorezova <nedorezova.emp@gmail.com>
2024-10-12 12:54:02 +0300

    changed application.properties, schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,23 @@
 CREATE TABLE Author (
-                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        id SERIAL PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
                         surname VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Book (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      id SERIAL PRIMARY KEY,
                       title VARCHAR(255) NOT NULL,
                       genre VARCHAR(255) NOT NULL,
-                      author_id INT,
-                      FOREIGN KEY (author_id) REFERENCES Author(id)
+                      author_id INTEGER REFERENCES Author(id)
 );
 
 CREATE TABLE Genre (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       id SERIAL PRIMARY KEY,
                        name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE BookGenre (
-                           book_id INT,
-                           genre_id INT,
-                           PRIMARY KEY (book_id, genre_id),
-                           FOREIGN KEY (book_id) REFERENCES Book(id),
-                           FOREIGN KEY (genre_id) REFERENCES Genre(id)
+                           book_id INTEGER REFERENCES Book(id),
+                           genre_id INTEGER REFERENCES Genre(id),
+                           PRIMARY KEY (book_id, genre_id)
 );
