d6ebf251f7a385b1a247a4dd65b62041f316e460
LenaNedorez <91312310+LenaNedorez@users.noreply.github.com>
2024-10-18 13:50:19 +0300

    Update AuthorDAOImpl.java

--- src/main/java/ru/nedorezova/repository/AuthorDAOImpl.java
+++ src/main/java/ru/nedorezova/repository/AuthorDAOImpl.java
@@ -9,103 +9,24 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Implementation of the AuthorDAO interface using JDBC to interact with a PostgreSQL database.
- */
-public class AuthorDAOImpl implements AuthorDAO {
+@Service
+public class AuthorService {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthorDAOImpl.class);
-    private final String jdbcUrl = "jdbc:postgresql://localhost:5432/library";
-    private final String jdbcUser = "postgres";
-    private final String jdbcPassword = "password";
-    private final String jdbcDriver = "org.postgresql.Driver";
+    private final AuthorRepository authorRepository;
 
-    public AuthorDAOImpl() {}
-
-    /**
-     * Establishes a connection to the PostgreSQL database.
-     *
-     * @return A Connection object representing the database connection.
-     */
-    protected Connection getConnection() {
-        Connection connection = null;
-
-        try {
-            Class.forName(jdbcDriver);
-            connection = DriverManager.getConnection(jdbcUrl,jdbcUser,jdbcPassword);
-        } catch (SQLException e) {
-            logger.error("Error connecting to database: ", e);
-        } catch (ClassNotFoundException e) {
-            logger.error("JDBC driver not found: {}", jdbcDriver, e);
-        }
-        return connection;
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
-    /**
-     * Retrieves all authors from the database.
-     *
-     * @return A list of Author objects representing all authors in the database.
-     */
-    @Override
     public List<Author> getAllAuthors() {
-        List <Author> authors = new ArrayList<>();
-        try(Connection connection = getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM authors");){
-            ResultSet rs = preparedStatement.executeQuery();
-            while (rs.next()) {
-                Integer id = rs.getInt("id");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                authors.add(new Author(id, name, surname));
-            }
-            rs.close();
-        } catch (SQLException e) {
-            logger.error("Error getting all authors: ", e);
-        }
-
-        return authors;
+        return authorRepository.findAll();
     }
 
-    /**
-     * Retrieves an author from the database by their ID.
-     *
-     * @param id The ID of the author to retrieve.
-     * @return An Author object representing the author with the specified ID.
-     */
-    @Override
     public Author getAuthorById(Integer id) {
-        Author author = new Author();
-        try(Connection connection = getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM authors WHERE id = ?");){
-            preparedStatement.setInt(1, id);
-            ResultSet rs = preparedStatement.executeQuery();
-            while (rs.next()) {
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                author = new Author(id, name, surname);
-            }
-            rs.close();
-        } catch (SQLException e) {
-            logger.error("Error getting author by id: {}", id, e);
-        }
-
-        return author;
+        return authorRepository.findById(id).orElse(null);
     }
 
-    /**
-     * Creates a new author in the database.
-     *
-     * @param author The Author object to create.
-     */
-    @Override
-    public void createAuthor(Author author) {
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO authors (name, surname) VALUES (?, ?)")) {
-            preparedStatement.setString(1, author.getName());
-            preparedStatement.setString(2, author.getSurname());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            logger.error("Error creating author: {}", author, e);
-        }
+    public Author createAuthor(Author author) {
+        return authorRepository.save(author);
     }
 }
