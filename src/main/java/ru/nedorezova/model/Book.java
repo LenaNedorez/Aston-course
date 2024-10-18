4ff6d7466a50e2f7cfbf7b3067fdbcdea332c004
LenaNedorez <91312310+LenaNedorez@users.noreply.github.com>
2024-10-18 12:53:45 +0300

    Update Book.java

--- src/main/java/ru/nedorezova/model/Book.java
+++ src/main/java/ru/nedorezova/model/Book.java
@@ -3,51 +3,18 @@ package ru.nedorezova.model;
 /**
  * Represents a book.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "books")
 public class Book {
+    
     private Integer id;
     private String title;
     private String genre;
     private Author author;
 
-    public Book(Integer id, String title, String genre, Author author) {
-        this.id = id;
-        this.title = title;
-        this.genre = genre;
-        this.author = author;
-    }
-
-    public Book() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
 }
