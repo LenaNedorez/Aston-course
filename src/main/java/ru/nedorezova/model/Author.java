09c268004cf61fa3eff769e2d26361e2aa0a3c63
LenaNedorez <91312310+LenaNedorez@users.noreply.github.com>
2024-10-18 12:52:14 +0300

    Update Author.java

--- src/main/java/ru/nedorezova/model/Author.java
+++ src/main/java/ru/nedorezova/model/Author.java
@@ -3,41 +3,19 @@ package ru.nedorezova.model;
 /**
  * Represents an author of a book.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "authors")
 public class Author {
-    private Integer id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private String surname;
-
-    public Author(Integer id, String name, String surname) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-    }
-
-    public Author() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+    
 }
