b2d0f875a5fd70ff372edf7d56d02abafcf9b933
LenaNedorez <91312310+LenaNedorez@users.noreply.github.com>
2024-10-18 12:54:05 +0300

    Update Genre.java

--- src/main/java/ru/nedorezova/model/Genre.java
+++ src/main/java/ru/nedorezova/model/Genre.java
@@ -5,40 +5,17 @@ import java.util.List;
 /**
  * Represents a genre.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "authors")
 public class Genre {
 
     private int id;
     private String name;
     private List<Book> books;
-
-    public Genre(int id, String name, List<Book> books) {
-        this.id = id;
-        this.name = name;
-        this.books = books;
-    }
-
-    public Genre() {
-    }
-
-    public Genre(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
